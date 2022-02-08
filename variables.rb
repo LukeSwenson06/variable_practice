81085aff84ac49fae56f7aab35f8051c298e672a
Luke Swenson <LukeSwenson95@outlook.com>
2022-02-07 20:26:46 -0700

    Annotate 3 lines

--- variables.rb
+++ variables.rb
@@ -1,10 +1,10 @@
-greeting = "Hello Everybody"
+greeting = "Hello Everybody" #variable greeting is declared and assigned to the string of "Hello Everybody"
 inquiry = "How are you doing?"
 my_feelings = "I am doing fantastic"
-age = 26
+age = 26 #Variable age is declared and assigned to the integer of "26"
 height = 60
 weight = 180
-money = 100.00
+money = 100.00 #Variable money is declared and assigned to the float of "100"
 miles_ran = 2.3
 have_money = true
 
